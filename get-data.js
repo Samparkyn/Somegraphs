a7f47e29c3d51ed552f7b2ad1b5de12c73a1ba62
Samparkyn <samparkyn101@gmail.com>
2018-06-10 18:19:26 +0200

    Add get-data file to read text file & write data to JSON

---
+++ get-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const filePath = __dirname + '/epa-http.txt';
+
+function processRawData(rawData) {
+  const data = [];
+  const textFileLines = rawData.split('\n');
+  for (let idx = 0; idx < textFileLines.length; idx++) {
+    const line = textFileLines[idx]
+    if(!line) {
+      continue
+    }
+    const [
+      host,
+      datetime,
+      method,
+      url,
+      protocolData,
+      response_code,
+      document_size
+    ] = line.replace(/"/g, '').split(' ')
+    const [day, hour, minute, second] = datetime.replace(/\[|\]/g, '').split(':')
+    const [protocol, protocol_version] = protocolData.split('/')
+    
+    const dataObject = {
+      host,
+      datetime: { day, hour, minute, second },
+      request: { method, url, protocol, protocol_version },
+      response_code,
+      document_size
+    }
+    
+    data.push(dataObject)
+  }
+  return data
+}
+
+const rawData = fs.readFileSync(filePath, 'utf8');
+const dataArray = processRawData(rawData);
+saveDataToJSON(dataArray);
+
+function saveDataToJSON(dataArray) {
+  fs.writeFile('data.json', JSON.stringify(dataArray), (err) => {
+    if (err) {
+      console.log('There was an error!', err);
+    }
+
+    console.log('data saved');
+  });
+}
+
+
