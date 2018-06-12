2df1f0f8d884764da72487429ec8398a2d8fdbf3
Samparkyn <samparkyn101@gmail.com>
2018-06-12 22:50:40 +0200

    Add functions to display data in charts

--- client/src/App.js
+++ client/src/App.js
@@ -28,48 +28,134 @@ class App extends Component {
           });
   }
 
+  calculateMethodPercentages(data){
+    const getRequests = []
+    const postRequests = []
+    const headRequests = []
+    const otherReqs = []
+    const methods = data.map(dataObject => {
+      const request = dataObject.request.method
+      if (request === "GET") {
+        getRequests.push(request)
+      } else if (request === "POST") {
+        postRequests.push(request)
+      } else if (request === "HEAD") {
+        headRequests.push(request)
+      } else {
+        otherReqs.push(request)
+      }
+    })
+    const totalGETReqs = getRequests.length
+    const totalPOSTReqs = postRequests.length
+    const totalHEADReqs = headRequests.length
+    const totalOtherReqs = otherReqs.length
+    const totalReqs = (totalGETReqs + totalPOSTReqs + totalHEADReqs + otherReqs)
+    const getReqPercent = ((totalGETReqs / 47748) * 100).toFixed(2)
+    const postReqPercent = ((totalPOSTReqs / 47748) * 100).toFixed(2)
+    const headReqPercent = ((totalHEADReqs / 47748) * 100).toFixed(2)
+    const otherReqPercent = ((totalOtherReqs / 47748) * 100).toFixed(2)
+
+
+    const totalRequestsObj = {
+      'GET': parseFloat(getReqPercent),
+      'POST': parseFloat(postReqPercent),
+      'HEAD': parseFloat(headReqPercent),
+      'OTHER': parseFloat(otherReqPercent)
+    }
+
+    return totalRequestsObj
+  }
+
   requestsPerMin(data){
+    const methods = this.calculateMethodPercentages(data)
     const options = {
       chart: {
         type: 'bar'
       },
       title: {
-          text: 'Fruit Consumption'
+          text: 'Distribution of HTTP Methods'
       },
       xAxis: {
-          categories: ['Apples', 'Bananas', 'Oranges']
+          categories: ['GET POST HEAD OTHER']
       },
       yAxis: {
           title: {
-              text: 'Fruit eaten'
+              text: '% of Reqs'
           }
       },
       series: [{
-          name: 'Jane',
-          data: [1, 0, 4]
+          name: 'GET',
+          data: [methods.GET]
       }, {
-          name: 'John',
-          data: [5, 7, 3]
+          name: 'POST',
+          data: [methods.POST]
+      }, {
+          name: 'HEAD',
+          data: [methods.HEAD]
+      }, {
+        name: 'OTHER',
+        data: [methods.OTHER]
       }]
     }
     return options;
   }
 
   HTTPMethodDistribution(data){
-
+    const methods = this.calculateMethodPercentages(data)
+    //total requests/sets of data = 47748
+    const options = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Distribution of HTTP methods'
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'GET',
+            y: methods.GET,
+        }, {
+            name: 'POST',
+            y: methods.POST,
+        }, {
+            name: 'HEAD',
+            y: methods.HEAD,
+          }, {
+            name: 'OTHER',
+            y: methods.OTHER
+        }]
+      }]
+    }
+    return options;
   }
 
-  HTTPCodeDistribution(data){
-
+  HTTPCodeDistribution(data) {
+    const distribution = data.reduce((acc, dataPoint) => {
+      const responseCode = dataPoint.response_code
+      if(acc[responseCode]) {
+        acc[responseCode]++
+        return acc
+      }
+      acc[responseCode] = 1
+      return acc
+    }, {})
+    console.log(distribution)
   }
 
   answerSizeDistribution(data){
-
+//pie chart
   }
 
   render() {
     const { data } = this.state
-
+    if (!data){
+      return <div>Loading..</div>
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -77,7 +163,10 @@ class App extends Component {
         </header>
         <p>Charts go here:</p>
         <div>
-          <Chart options={this.requestsPerMin(data)} />
+          <Chart className="chart" options={this.requestsPerMin(data)} />
+          <Chart className="chart" options={this.HTTPMethodDistribution(data)} />
+          <Chart className="chart" options={this.HTTPCodeDistribution(data)} />
+          <Chart className="chart" options={this.requestsPerMin(data)} />
         </div>
       </div>
     );
