befd07dcb3a92934616396546248c713fee7a58e
Samparkyn <samparkyn101@gmail.com>
2018-06-14 21:17:34 +0200

    Move files to server folder

--- server/process-data.js
+++ server/process-data.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
 const filePath = __dirname + '/epa-http.txt';
 
+let skipCount = 0, processedCount = 0;
 function processRawData(rawData) {
   const data = [];
   const textFileLines = rawData.split('\n');
+  console.log('Processing all', textFileLines.length, 'lines of data...');
+
   for (let idx = 0; idx < textFileLines.length; idx++) {
     const line = textFileLines[idx]
     if(!line) {
-      continue
+      continue;
     }
+
+    const dataPoints = line.replace(/"/g, '').split(' ');
+    if (dataPoints.length !== 7) {
+      console.log('Corrupt data found at line', idx + 1, 'skipping...');
+      skipCount++;
+      continue;
+    }
+
     const [
       host,
       datetime,
@@ -17,11 +28,10 @@ function processRawData(rawData) {
       protocolData,
       response_code,
       document_size
-    ] = line.replace(/"/g, '').split(' ')
+    ] = dataPoints;
     const [day, hour, minute, second] = datetime.replace(/\[|\]/g, '').split(':')
     const [protocol, protocol_version] = protocolData.split('/')
-    console.log(url)
-    
+
     const dataObject = {
       host,
       datetime: { 
@@ -40,23 +50,26 @@ function processRawData(rawData) {
       document_size
     }
     
+    processedCount++;
     data.push(dataObject)
   }
   return data
 }
 
-const rawData = fs.readFileSync(filePath, 'utf8');
-const dataArray = processRawData(rawData);
-saveDataToJSON(dataArray);
-
 function saveDataToJSON(dataArray) {
-  fs.writeFile('data.json', JSON.stringify(dataArray), (err) => {
+  fs.writeFile(__dirname + '/data.json', JSON.stringify(dataArray), (err) => {
     if (err) {
       console.log('There was an error!', err);
     }
-
-    console.log('data saved');
+    process.exit();
   });
 }
 
-
+const rawData = fs.readFileSync(filePath, 'utf8');
+const dataArray = processRawData(rawData);
+saveDataToJSON(dataArray);
+console.log(
+  'Successfully processed the data\n',
+  processedCount, 'lines processed.\n',
+  skipCount, 'lines skipped due to incomplete data.'
+);
